0a3980f436557023843d53db02ba8ab3ea21e813
Yony <arleybri18@gmail.com>
2019-04-24 09:24:36 -0500

    add more solutions

--- solution.rb
+++ solution.rb
@@ -8,3 +8,27 @@ get '/' do
          end
   erb :index
 end
+
+
+# another solution is 
+
+# dentro del archivo solution.rb
+
+# require 'sinatra'
+
+# get '/' do
+#   @number = params[:number]
+#   erb :index
+# end
+# <!--  Dentro del archivo index.erb -->
+
+# <% if @number %>
+#   <h1>Mi cuenta es : <%= @number %></h1>
+# <% else %>
+#   <h1>Mi cuenta es : 0</h1>
+#   <% @number = 0 %>
+# <% end %>
+# <form action="/">
+#   <input type="hidden" value="<%= @number.to_i + 1 %>" name="number">
+#   <input type="submit" value="Suma">
+# </form>
